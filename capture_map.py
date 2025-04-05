ac704b8e362f3c60796485dbe7c27d92a87eae5a
Juhan Oskar Hennoste <juhanoskar@gmail.com>
2025-04-05 03:47:11 +0300

    Add 100 m grid lines to base map

--- capture_map.py
+++ capture_map.py
@@ -1,10 +1,10 @@
+import math
 import pyautogui
 import pygetwindow
+from PIL import ImageDraw
 
 # Usage: Open in-game map in Arma Reforger and run this.
 # Note: For a higher resolution capture use a virtual monitor with high resolution.
-
-# Working area: 110-1870, 40-1010
 
 def drag(x_from, y_from, x_to, y_to):
     pyautogui.moveTo(x_from, y_from)
@@ -30,4 +30,17 @@ if __name__ == '__main__':
     # Capture map
     pyautogui.moveTo(width // 48, width // 48)
     pyautogui.sleep(0.5)
-    pyautogui.screenshot('full_map.png', ((width - height) // 2, 0, height, height))
+    map_image = pyautogui.screenshot(None, ((width - height) // 2, 0, height, height))
+
+    # Draw 100 m grid lines
+    grid_interval = height / 10240 * 100
+    draw = ImageDraw.Draw(map_image, 'RGBA')
+    for i in range(1, math.ceil(height / grid_interval)):
+        if (i % 10 == 0):
+            continue
+        line_offset = round(i * grid_interval)
+        draw.line((line_offset, 0, line_offset, height), (50, 50, 50, 100))
+        draw.line((0, height - line_offset, height, height - line_offset), (50, 50, 50, 100))
+
+    # Save final result
+    map_image.save('full_map.png')    
